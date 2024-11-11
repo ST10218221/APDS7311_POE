bc6ef820787276b405a7eabe29cac9ade05608b5
ST10088734 <129064031+ST10088734@users.noreply.github.com>
2024-11-11 20:03:45 +0200

    Delete backend directory

--- backend/config.js
+++
@@ -1,9 +0,0 @@
-/*
-The following config code was taken from a video by Isaac Leshaba:
-Leshaba, I., 2024. VC Learn, MERN Backend Video. [Online] 
-Available at: https://myvc.iielearn.ac.za/ultra/courses/_216899_1/cl/outline
-[Accessed 26 September 2024].
-*/
-
-import dotenv from "dotenv";
-dotenv.config();
